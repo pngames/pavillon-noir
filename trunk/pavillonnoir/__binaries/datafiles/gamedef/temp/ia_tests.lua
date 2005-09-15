1b31212d01aecc56666977e77ebfa4f24fd8bd8c
marti <marti@0d441a28-bafb-0310-9ec5-ae81da3c9b46>
2005-09-15 15:19:35 +0000

    ajout d'une map pour le PF. le PF marche !

--- trunk/pavillonnoir/__binaries/datafiles/gamedef/temp/ia_tests.lua
+++ trunk/pavillonnoir/__binaries/datafiles/gamedef/temp/ia_tests.lua
@@ -2,7 +2,7 @@ pnprint("entering ia_tests.lua\n")
 if (gameMap.entities.className.Character ~= nil) then
 	for id, entity in pairs(gameMap.entities.className.Character) do
 		pnprint(entity.id)
-		local aPoint = PNPoint:new_local(4900.0, 0.0, 4900.0)
+		local aPoint = PNPoint:new_local(3700.0, 0.0, 4900.0)
 		entity:moveTo(aPoint)
 		pnprint("\n")
 	end	
