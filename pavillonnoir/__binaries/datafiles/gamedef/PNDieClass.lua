48d372be5f8e3b53790d9554e1a6a0b8201a24b4
Mattia Bordoni <mattia.bordoni@gmail.com>
2005-11-24 16:48:44 +0000

    Minor modification in PNDieClass.lua to improve the random value generated.

--- pavillonnoir/__binaries/datafiles/gamedef/PNDieClass.lua
+++ pavillonnoir/__binaries/datafiles/gamedef/PNDieClass.lua
@@ -2,13 +2,18 @@ function PNDieClass()
 	pnprint("PNDieClass creating\n")
     local OBJ = nil
     OBJ.className="PNDieClass"
-	-- Seed init based on system's time
+	-- Seed's init based on the system's time
 	math.randomseed( os.time() )
     
-    function OBJ:getVal()
-    -- Returns pseudo random value between 1 and 6
-		return (math.random(6));
-	end    
+		function OBJ:getVal()
+			-- Returns pseudo random value between 1 and 6
+			-- 3 random are made du to warning in lua's doc: 
+			-- "beware! The first random number you get is not really 'randomized' (at least in Windows 2k).
+			-- To get better pseudo-random number just pop some random number before using them for real"
+			math.random(6)
+			math.random(6)
+			return (math.random(6));
+		end    
 	
 	return  OBJ
 end
