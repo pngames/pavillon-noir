0b024af2b88a515b23e8d9a435c56ebeaee0049e
tuska <tuska@0d441a28-bafb-0310-9ec5-ae81da3c9b46>
2006-02-10 17:11:29 +0000

    * create map for advanced physics tests * add test project (with event manager tests for now)

---
+++ trunk/pavillonnoir/__src/test_pncommon/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "tests.h"
+
+int main(int ac, char *av[])
+{
+  test_events();
+
+  char c;
+  std::cout << "Appuyez sur une touche pour continuer" << std::endl;
+  fread(&c, 1, 1, stdin);
+
+  return 0;
+}
